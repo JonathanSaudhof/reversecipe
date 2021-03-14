0e7b9a2a9aafb9619742e6aa08ee1fb9e058a331
Jonathan <Jonathan@jonathansaudhof.de>
2021-03-14 17:32:46 +0100

    changed to container on master layout

--- resources/views/components/layout/master.blade.php
+++ resources/views/components/layout/master.blade.php
@@ -1,5 +1,5 @@
 <div class="min-h-screen bg-gray-50 py-8">
-    <div class="w-3/5 relative mx-auto flex flex-col items-center">
+    <div class="container relative mx-auto flex flex-col items-center">
         {{ $slot }}
     </div>
 </div>
